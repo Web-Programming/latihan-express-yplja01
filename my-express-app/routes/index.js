f452ca61b296e940fcbedc876a5bc5755b9035cc
yplja01 <erikafebriyani_2226240050@mhs.mdp.ac.id>
2024-10-02 09:31:17 +0700

    Pertemuan 7
    menambahkan folder my-express-app

---
+++ my-express-app/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next){
+  res.render("about", { title: 'About Us'});
+});
+
+router.get('/kontak', function(req, res, next){
+  res.render("kontak", { title: 'Hubungi kami'});
+});
+
+module.exports = router;
